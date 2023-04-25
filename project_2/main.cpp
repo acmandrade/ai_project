035322a40a9e267358af3f74cb938ae19efcbc9b
acmandrade <carol02_miranda@hotmail.com>
2023-04-25 01:24:52 -0500

    final project

--- project_2/main.cpp
+++ project_2/main.cpp
@@ -23,8 +23,8 @@ int distance(const std::vector<std::vector<int>>& state, const std::vector<std::
         for (int j = 0; j < n; ++j) {
             int value = state[i][j];
             if (value != 0) {
-                int goal_i = (value - 1) / n;
-                int goal_j = (value - 1) % n;
+                int goal_i = (value - 1) / n;  // row
+                int goal_j = (value - 1) % n;  // column
                 distance += abs(i - goal_i) + abs(j - goal_j);
             }
         }
@@ -32,18 +32,23 @@ int distance(const std::vector<std::vector<int>>& state, const std::vector<std::
     return distance;
 }
 
+std::vector<std::vector<int>> tile_weights = {
+    {1, 2, 3},
+    {4, 5, 6},
+    {7, 8, 9}
+};
+
 //Implement unique heuristic here
 int diagonal_conflict(const std::vector<std::vector<int>>& state, const std::vector<std::vector<int>>& goal) {
     int conflict_count = 0;
     int n = state.size();
-    
+
     for (int i = 0; i < n; ++i){
         for (int j = 0; j < n; ++j){
             int value = state[i][j];
             if (value != 0) {
                 int goal_i = (value - 1) / n;
                 int goal_j = (value - 1) % n;
-                
                 if ((i - j == goal_i - goal_j || i + j == goal_i + goal_j) && (i != goal_i || j != goal_j)) {
                     conflict_count++;
                 }
@@ -51,6 +56,49 @@ int diagonal_conflict(const std::vector<std::vector<int>>& state, const std::vec
         }
     }
     return  conflict_count * 2;
+}
+
+int orderly(const std::vector<std::vector<int>>& state, const std::vector<std::vector<int>>& goal) {
+    int away = 0;
+    int n = state.size();
+    std::vector<std::vector<int>> weights =
+    {
+        {10, 9, 8},
+        {3, 0, 7},
+        {4, 5, 6}
+    };
+    for (int i = 0; i < n; ++i)
+    {
+        for (int j = 0; j < n; ++j)
+        {
+            int value = state[i][j];
+            int addative = weights[i][j];
+            if (value != 0)
+            {
+                int goal_i = (value - 1) / n;
+                int goal_j = (value - 1) % n;
+                
+                away += addative*(abs(i - goal_i) + abs(j - goal_j));
+            }
+        }
+    }
+    return away;
+}
+
+double euc_distance(const std::vector<std::vector<int>>& state, const std::vector<std::vector<int>>& goal) {
+    double distance = 0;
+    int n = state.size();
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n; ++j) {
+            int value = state[i][j];
+            if (value != 0) {
+                int goal_i = (value - 1) / n;
+                int goal_j = (value - 1) % n;
+                distance += sqrt(pow((i-goal_i),2) + pow((j-goal_j),2));
+            }
+        }
+    }
+    return distance;
 }
 
 bool is_goal(const std::vector<std::vector<int>>& state, const std::vector<std::vector<int>>& goal) {
@@ -69,21 +117,21 @@ std::vector<std::vector<std::vector<int>>> generate_successors(const std::vector
             }
         }
     }
-
+    
     std::vector<std::vector<std::vector<int>>> successors;
     std::vector<std::pair<int, int>> directions = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
     
     for (const auto& direction : directions) {
         int new_i = i0 + direction.first;
         int new_j = j0 + direction.second;
-
+        
         if (new_i >= 0 && new_i < n && new_j >= 0 && new_j < n) {
             std::vector<std::vector<int>> new_state = state;
             std::swap(new_state[i0][j0], new_state[new_i][new_j]);
             successors.push_back(new_state);
         }
     }
-
+    
     return successors;
 }
 
@@ -96,20 +144,6 @@ void print_puzzle(const std::vector<std::vector<int>>& puzzle) {
     }
 }
 
-//PuzzleNode* create_puzzle_node(const std::vector<std::vector<int>>& state, const std::vector<std::vector<int>>& goal) {
-//    PuzzleNode* node = new PuzzleNode();
-//    node->state = state;
-//    node->g = 0;
-//    node->h = distance(state, goal);
-//    node->f = node->g + node->h;
-//    node->parent = nullptr;
-//
-//    //increments the node counter
-//    nodes_generated++;
-//
-//    return node;
-//}
-
 PuzzleNode* create_puzzle_node(const std::vector<std::vector<int>>& state, const std::vector<std::vector<int>>& goal, int heuristic_choice) {
     PuzzleNode* node = new PuzzleNode();
     node->state = state;
@@ -119,8 +153,12 @@ PuzzleNode* create_puzzle_node(const std::vector<std::vector<int>>& state, const
         node->h = distance(state, goal);
     } else if (heuristic_choice == 2) {
         node->h = diagonal_conflict(state, goal);
-    } else {
-        std::cout << "Invalid choice. Defaulting to Manhattan distance." << std::endl;
+    } else if (heuristic_choice == 3) {
+        node->h = orderly(state, goal);
+    } else if (heuristic_choice == 4) {
+        node->h = euc_distance(state, goal);
+    }else {
+        std::cout << "Invalid choice. Defaulting to distance." << std::endl;
         node->h = distance(state, goal);
     }
     node->f = node->g + node->h;
@@ -135,12 +173,12 @@ PuzzleNode* create_puzzle_node(const std::vector<std::vector<int>>& state, const
 std::vector<PuzzleNode*> extract_path(PuzzleNode* goal_node) {
     std::vector<PuzzleNode*> path;
     PuzzleNode* current_node = goal_node;
-
+    
     while (current_node != nullptr) {
         path.push_back(current_node);
         current_node = current_node->parent;
     }
-
+    
     std::reverse(path.begin(), path.end());
     return path;
 }
@@ -165,30 +203,30 @@ struct container_hash {
     }
 };
 
-//void propagate_cost_downward(PuzzleNode* node, int g_propagated, const std::vector<std::vector<int>>& goal) {
-//    if (g_propagated + node->h < node->f) {
-//        node->g = g_propagated;
-//        node->f = g_propagated + node->h;
-//
-//        for (const auto& successor_state : generate_successors(node->state)) {
-//            PuzzleNode* successor = create_puzzle_node(successor_state, goal);
-//            if (node->parent != nullptr && node->parent->state == successor->state) {
-//                continue;
-//            }
-//            propagate_cost_downward(successor, g_propagated + 1, goal);
-//            delete successor;
-//        }
-//    }
-//}
+void propagate_cost_downward(PuzzleNode* node, int g_propagated, const std::vector<std::vector<int>>& goal, int heuristic_choice) {
+    if (g_propagated + node->h < node->f) {
+        node->g = g_propagated;
+        node->f = g_propagated + node->h;
+        
+        for (const auto& successor_state : generate_successors(node->state)) {
+            PuzzleNode* successor = create_puzzle_node(successor_state, goal, heuristic_choice);
+            if (node->parent != nullptr && node->parent->state == successor->state) {
+                continue;
+            }
+            propagate_cost_downward(successor, g_propagated + 1, goal, heuristic_choice);
+            delete successor;
+        }
+    }
+}
 
 std::vector<PuzzleNode*> a_star(PuzzleNode* initial_node, const std::vector<std::vector<int>>& goal, int heuristic_choice) {
     auto cmp = [](PuzzleNode* a, PuzzleNode* b) { return a->f > b->f; };
     
     //Stores nodes in the OPEN list and is ordered by f' value
     std::priority_queue<PuzzleNode*, std::vector<PuzzleNode*>, decltype(cmp)> open(cmp);
-    //Stores nodes in the CLOSEDE list
+    //Stores nodes in the CLOSED list
     std::unordered_set<std::vector<std::vector<int>>, container_hash> closed;
-
+    
     open.push(initial_node);
     //Iterates until a goal node is found or no solution
     while (!open.empty()) {
@@ -197,34 +235,43 @@ std::vector<PuzzleNode*> a_star(PuzzleNode* initial_node, const std::vector<std:
         PuzzleNode* best_node = open.top();
         nodes_expanded++;
         open.pop();
-        //CHECKS TO SEE I F BESTNODE IS GOAL NODE
-        //checks current stat is the goal state
+        //CHECKS TO SEE IF BESTNODE IS GOAL NODE
+        //checks current state is the goal state
         if (is_goal(best_node->state, goal)) {
             return extract_path(best_node);
         }
-
+        
         closed.insert(best_node->state);
         
         //generates successors of the current state
         auto successors = generate_successors(best_node->state);
-
+        
         for (const auto& successor_state : successors) {
             if (closed.find(successor_state) != closed.end()) {
                 continue;
             }
-
+            
             PuzzleNode* successor_node = create_puzzle_node(successor_state, goal, heuristic_choice);
             
+            //set best_node to successor
             successor_node->parent = best_node;
-            //set best_nodde to successor
             successor_node->g = best_node->g + 1;
             //compute g & f'
             successor_node->f = successor_node->g + successor_node->h;
             
-            open.push(successor_node);
+            if (closed.find(successor_state) != closed.end()) {
+                auto it = std::find_if(closed.begin(), closed.end(), [&successor_state](const auto& old_state) {
+                    return old_state == successor_state;
+                });
+                
+                if (it != closed.end()) {
+                    propagate_cost_downward(successor_node, best_node->g + 1, goal, heuristic_choice);
+                }
+            } else {
+                open.push(successor_node);
+            }
         }
     }
-
     return {}; // Return an empty vector if no solution is found.
 }
 
@@ -240,14 +287,18 @@ int get_tree_depth(const std::vector<PuzzleNode*>& path) {
     return path.size() - 1;
 }
 
+int get_total_path(const std::vector<PuzzleNode*>& path) {
+    return path.size();
+}
+
 double get_b(const std::vector<PuzzleNode*>& path){
     int ng = get_nodes();
     int d = get_tree_depth(path);
-
+    
     if(d == 0){
         return 0.0;
     }
-
+    
     return static_cast<double>(ng) / d;
 }
 
@@ -267,7 +318,7 @@ int main() {
         {4, 0, 8},
         {7, 5, 3}
     };
-
+    
     std::vector<std::vector<int>> goal_state = {
         {1, 2, 3},
         {8, 0, 4},
@@ -282,7 +333,7 @@ int main() {
     std::cout << "Enter : ";
     std::cin >> choice;
     std::cout << std::endl;
-
+    
     std::vector<std::vector<int>> initial_state;
     switch (choice) {
         case 1:
@@ -300,33 +351,36 @@ int main() {
     std::cout << "Choose a heuristic: " << std::endl;
     std::cout << "1. A* Final" << std::endl;
     std::cout << "2. Diagonal conflict" << std::endl;
+    std::cout << "3. Orderly" << std::endl;
+    std::cout << "4. Euclidean distance" << std::endl;
     std::cout << "Enter: ";
     std::cin >> heuristic_choice;
     std::cout << std::endl;
     
-    if(heuristic_choice != 1 && heuristic_choice != 2) {
+    if(heuristic_choice != 1 && heuristic_choice != 2 && heuristic_choice != 3 && heuristic_choice != 4) {
         std::cout << "Invalid choice. Using A* Final" << std::endl;
         heuristic_choice = 1;
     }
     
     auto start_time = std::chrono::high_resolution_clock::now();
-
+    
     PuzzleNode* initial_node = create_puzzle_node(initial_state, goal_state, heuristic_choice);
     std::vector<PuzzleNode*> solution_path = a_star(initial_node, goal_state, heuristic_choice);
-
+    
     auto end_time = std::chrono::high_resolution_clock::now();
     auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end_time - start_time).count();
-
+    
     if (!solution_path.empty()) {
-        std::cout << "Solution found in " << duration << " ms:\n\n";
         print_path(solution_path);
+        std::cout << "Solution found in " << duration << " ms:" << std::endl;
         std::cout << "Total nodes generated: " << get_nodes() << std::endl;
         std::cout << "Total nodes expanded: " << get_nodes_expanded() << std::endl;
         std::cout << "Depth of Tree: " << get_tree_depth(solution_path) << std::endl;
         std::cout << "Effective branching factor b*: " << get_b(solution_path) << std::endl;
+        std::cout << "Total cost: " << get_total_path(solution_path) << std::endl;
     } else {
-        std::cout << "No solution found.\n";
+        std::cout << "No solution found." << std::endl;
     }
-
+    
     return 0;
 }
